f9cb50415fd5feeaf8d7ae637272dfd7c048b149
SANAYEE <dudtkd0219@gmail.com>
2023-01-26 22:05:27 +0900

    회원 가입 테스트 코드

---
+++ src/test/java/com/bestcommerce/customer/service/AccountServiceTest.java
@@ -0,0 +1,28 @@
+package com.bestcommerce.customer.service;
+
+import com.bestcommerce.customer.domain.Customer;
+import com.bestcommerce.customer.service.account.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AccountServiceTest {
+
+    @Autowired
+    private AccountService accountService;
+
+
+    @Test
+    public void joinTestCase01() throws Exception{
+        Customer expected = new Customer(1,"dudtkd0219@gmail.com","dudtkd0219","박영상","010-0000-0000","19960826",'Y');
+
+        accountService.save(expected);
+
+        Customer actual = accountService.getOneCustomerInfo(expected.getCu_id());
+
+        assertEquals(expected,actual);
+    }
+}
