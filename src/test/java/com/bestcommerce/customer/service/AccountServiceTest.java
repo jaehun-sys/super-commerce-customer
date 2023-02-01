9fe61b2ac2e798e6a3f67c34e7738f66e6ecb612
jaehun-sys <us03098@gmail.com>
2023-02-02 00:42:43 +0900

    계정정보 저장 DisplayName 어노테이션 임시

--- src/test/java/com/bestcommerce/customer/service/AccountServiceTest.java
+++ src/test/java/com/bestcommerce/customer/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.bestcommerce.customer.service;
 
 import com.bestcommerce.customer.domain.Customer;
 import com.bestcommerce.customer.service.account.AccountService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ public class AccountServiceTest {
 
 
     @Test
+    @DisplayName("계정 정보 저장이 가능해야 한다.")
     public void joinTestCase01() throws Exception{
         Customer expected = new Customer("dudtkd0219@gmail.com","dudtkd0219","박영상","010-0000-0000","19960826",'Y');
 
