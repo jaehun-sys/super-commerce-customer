e32582abae0246c0dbd83c3c23e5226ef5b912ca
SANAYEE <dudtkd0219@gmail.com>
2023-03-08 23:54:05 +0900

    QueryDsl 레이어 분리

--- src/main/java/com/bestcommerce/customer/service/cart/CartService.java
+++ src/main/java/com/bestcommerce/customer/service/cart/CartService.java
@@ -3,29 +3,22 @@ package com.bestcommerce.customer.service.cart;
 import com.bestcommerce.customer.domain.*;
 import com.bestcommerce.customer.dto.CartItemDto;
 import com.bestcommerce.customer.repository.domain.CartRepository;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.bestcommerce.customer.repository.support.CartRepositorySupport;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-import static com.bestcommerce.customer.domain.QCart.cart;
-import static com.bestcommerce.customer.domain.QCustomer.customer;
-import static com.bestcommerce.customer.domain.QProduct.product;
-import static com.bestcommerce.customer.domain.QSize.size;
 
 @Service
 public class CartService {
 
     private final CartRepository cartRepository;
 
-    private final EntityManager entityManager;
-    private JPAQueryFactory queryFactory;
+    private final CartRepositorySupport cartRepositorySupport;
 
-    public CartService(CartRepository cartRepository, EntityManager entityManager){
+    public CartService(CartRepository cartRepository, CartRepositorySupport cartRepositorySupport){
         this.cartRepository = cartRepository;
-        this.entityManager = entityManager;
+        this.cartRepositorySupport = cartRepositorySupport;
     }
 
     public void putProductToCart(Size size, Customer customer, Product product, int productCount){
@@ -38,28 +31,6 @@ public class CartService {
     }
 
     public List<CartItemDto> practice(){
-        queryFactory = new JPAQueryFactory(entityManager);
-
-        List<CartItemDto> result =
-                queryFactory.select(Projections.constructor(CartItemDto.class,
-                                customer.cuId.as("customerId"),
-                                customer.cuName.as("customerName"),
-                                customer.cuEmail.as("customerEmail"),
-                                product.productId.as("productId"),
-                                product.productName.as("productName"),
-                                product.productCost.as("productCost"),
-                                product.sellerId.as("sellerId"),
-                                product.thumbPath.as("thumbnailPath"),
-                                product.deliveryCost.as("deliveryCost"),
-                                size.sizeId.as("sizeId"),
-                                size.measureId.as("measureId"),
-                                size.measureName.as("measureName"),
-                                size.contentId.as("contentId"),
-                                size.contentName.as("contentName"),
-                                size.sizeValue.as("sizeValue")))
-                .from(cart).innerJoin(cart.customer, customer).innerJoin(cart.product, product).innerJoin(cart.size, size)
-                .where(customer.cuId.eq(1L)).fetch();
-
-        return result;
+        return cartRepositorySupport.getCartItemDtoList();
     }
 }
