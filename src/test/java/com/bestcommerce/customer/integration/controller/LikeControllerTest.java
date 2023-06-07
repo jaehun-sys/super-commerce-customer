fc6935a60978c5bd61a3cd5418578e58bfff297d
SANAYEE <dudtkd0219@gmail.com>
2023-06-08 00:36:03 +0900

    spring rest docs 적용

--- src/test/java/com/bestcommerce/customer/integration/controller/LikeControllerTest.java
+++ src/test/java/com/bestcommerce/customer/integration/controller/LikeControllerTest.java
@@ -6,18 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class LikeControllerTest {
 
     @Autowired
@@ -29,9 +36,12 @@ public class LikeControllerTest {
     @Autowired
     private TestUtilService testUtilService;
 
+    @RegisterExtension
+    final RestDocumentationExtension restDocumentation = new RestDocumentationExtension("build/generated-snippets");
+
     @BeforeEach
-    void initial() throws Exception {
-        mockMvc = testUtilService.loginWithJwtToken(mockMvc,objectMapper);
+    void initial(RestDocumentationContextProvider restDocumentation) throws Exception {
+        mockMvc = testUtilService.loginWithJwtToken(mockMvc,objectMapper,restDocumentation);
     }
 
     @DisplayName("상품 좋아요 및 취소 테스트")
@@ -43,6 +53,9 @@ public class LikeControllerTest {
         String content = objectMapper.writeValueAsString(dto);
 
         mockMvc.perform(post("/like/product").contentType(MediaType.APPLICATION_JSON).content(content))
+                .andDo(document("like/productLikeOrCancel",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());
 
--- src/test/java/com/bestcommerce/customer/util/TestUtilService.java
+++ src/test/java/com/bestcommerce/customer/util/TestUtilService.java
@@ -3,15 +3,19 @@ package com.bestcommerce.customer.util;
 import com.bestcommerce.member.dto.MemberLoginDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -26,6 +30,9 @@ public class TestUtilService {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @RegisterExtension
+    final RestDocumentationExtension restDocumentation = new RestDocumentationExtension("custom");
+
     public MockMvc loginWithJwtToken(MockMvc mockMvc, ObjectMapper objectMapper) throws Exception{
 
         String result = mockMvc.perform(post("/member/login")
@@ -36,6 +43,21 @@ public class TestUtilService {
         String token = new JSONObject(result).getString("accessToken");
 
         return MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .defaultRequest(get("/").header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .defaultRequest(post("/").header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .build();
+    }
+
+    public MockMvc loginWithJwtToken(MockMvc mockMvc, ObjectMapper objectMapper, RestDocumentationContextProvider restDocumentation) throws Exception{
+
+        String result = mockMvc.perform(post("/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new MemberLoginDto(testId,testPwd))))
+                .andReturn().getResponse().getContentAsString();
+
+        String token = new JSONObject(result).getString("accessToken");
+
+        return MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(documentationConfiguration(restDocumentation))
                 .defaultRequest(get("/").header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .defaultRequest(post("/").header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .build();
